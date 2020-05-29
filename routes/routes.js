d9b9cab282c874dd6fa3e3532fcc52ba1ba674f6
MARIO PALOMO <mariofpalomoa@gmail.com>
2020-05-29 18:13:33 -0500

    v2
    v2

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('world/inicio', { title: 'Express' });
+  res.render('world/inicio');
+});
+
+router.get('/servicios', function(req, res, next) {
+  res.render('world/servicios', { tema: req.query.tema });
+});
+
+router.get('/temas', function(req, res, next) {
+  res.render('world/temas');
+});
+
+router.get('/detalle', function(req, res, next) {
+  res.render('world/temaDetalle', { tema: req.query.id });
+});
+
+router.get('/trabaja', function(req, res, next) {
+  res.render('world/trabaja', { tema: req.query.id });
+});
+
+router.get('/linea', function(req, res, next) {
+  res.render('world/linea', { tema: req.query.id });
 });
 
 module.exports = router;
