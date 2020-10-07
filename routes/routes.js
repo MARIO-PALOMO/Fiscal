db87a733e3d37391b4dae2fd61edcdb52807a2a9
MARIO PALOMO <mariofpalomoa@gmail.com>
2020-10-07 11:51:30 -0500

    v2
    v2

--- routes/routes.js
+++ routes/routes.js
@@ -21,6 +21,33 @@ router.get('/trabaja', function(req, res, next) {
   res.render('world/trabaja', { tema: req.query.id });
 });
 
+router.get('/contabilidad', function(req, res, next) {
+  res.render('world/contabilidad', { tema: req.query.id });
+});
+
+
+router.get('/auditorias', function(req, res, next) {
+  res.render('world/auditorias', { tema: req.query.id });
+});
+
+
+router.get('/capacitacion', function(req, res, next) {
+  res.render('world/capacitacion', { tema: req.query.id });
+});
+
+
+router.get('/impuestos', function(req, res, next) {
+  res.render('world/impuestos', { tema: req.query.id });
+});
+
+router.get('/nomina', function(req, res, next) {
+  res.render('world/nomina', { tema: req.query.id });
+});
+
+router.get('/asesoria', function(req, res, next) {
+  res.render('world/asesorias', { tema: req.query.id });
+});
+
 router.get('/linea', function(req, res, next) {
   res.render('world/linea', { tema: req.query.id });
 });
